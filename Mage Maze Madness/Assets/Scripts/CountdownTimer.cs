4a5293f3a9f1a3bbc6ae7e0c6eaea308bb2b2e16
AlisonBrannon <60676887+AlisonBrannon@users.noreply.github.com>
2020-11-18 15:21:09 -0500

    Added CountdownTimer and fixed a few UI Things.

---
+++ Mage Maze Madness/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    [Tooltip("Set the time for the countdown in seconds.")]
+    public float timeRemaining = 10;
+
+    [Tooltip("Is the timer turned on.")]
+    public bool timerIsRunning = true;
+
+    [Tooltip("Set a UI text in this slot. Make sure the UI text is child to the Canvas.")]
+    public Text timeText;
+
+    [Tooltip("Set the scene index number for the scene you want for the player to go to. ")]
+    public int SceneIndex;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                SceneManager.LoadScene(SceneIndex);
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}

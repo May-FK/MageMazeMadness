0b9ece6781383b0c5b7ec83d7b47fe8ee80edc9d
HeyItzFranklin <72268144+HeyItzFranklin@users.noreply.github.com>
2020-11-19 11:38:12 -0500

    Changed order of variables in wind mage for ocd purposes

--- Mage Maze Madness/Assets/Scripts/WindMage.cs
+++ Mage Maze Madness/Assets/Scripts/WindMage.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class WindMage : BaseMage
 {
-    public bool hasOrb;
     public bool isWindMage;
+    public bool hasOrb;
+
     public float jumpSpeed = 20.0f;
     public PlayerController pc;
 
-    public Material[] MageC = new Material[6];
+    public Material[] windC = new Material[6];
     Material[] mats;
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (isWindMage == true)
@@ -43,12 +45,12 @@ public class WindMage : BaseMage
     void WindRobes()
     {
         mats = Player.GetComponent<MeshRenderer>().materials;
-        mats[0] = MageC[0];
-        mats[1] = MageC[1];
-        mats[2] = MageC[2];
-        mats[3] = MageC[3];
-        mats[4] = MageC[4];
-        mats[5] = MageC[5];
+        mats[0] = windC[0];
+        mats[1] = windC[1];
+        mats[2] = windC[2];
+        mats[3] = windC[3];
+        mats[4] = windC[4];
+        mats[5] = windC[5];
         Player.GetComponent<MeshRenderer>().materials = mats;
 
     }

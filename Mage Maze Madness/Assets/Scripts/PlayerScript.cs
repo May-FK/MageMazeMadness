31d90e2bb5546d5c35ae411d0c87b7abc1d1a689
HeyItzFranklin <72268144+HeyItzFranklin@users.noreply.github.com>
2020-12-02 20:38:06 -0500

    Scipts for spawning the player

---
+++ Mage Maze Madness/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerScript : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected == true)
+        {
+            
+            PlayerController playerController = GetComponent<PlayerController>();
+            CharacterController characterController = GetComponent<CharacterController>();
+
+            playerController.enabled = false;
+            characterController.enabled = false;
+
+
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Mage Maze Madness/Assets/Scripts/playerCreator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Photon.Pun.Demo.PunBasics;
+public class playerCreator : MonoBehaviourPunCallbacks
+{
+    public GameObject theMage;
+
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(this.theMage.name, new Vector3(0f, 10f, 0f), Quaternion.identity);
+        
+    }
+
+}

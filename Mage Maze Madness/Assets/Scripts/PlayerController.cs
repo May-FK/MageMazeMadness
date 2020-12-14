e92208b98f269f42a97fd8f59a8b339727e2fbdd
Nate12503 <nathanshussey@gmail.com>
2020-12-14 16:53:10 -0500

    Update PlayerController.cs

--- Mage Maze Madness/Assets/Scripts/PlayerController.cs
+++ Mage Maze Madness/Assets/Scripts/PlayerController.cs
@@ -54,11 +54,11 @@ public class PlayerController : MonoBehaviour
             Vector3 forward = transform.TransformDirection(Vector3.forward).normalized;
             Vector3 right = transform.TransformDirection(Vector3.right).normalized;
 
-            float curSpeedX = speed * Input.GetAxis("Vertical");
-            float curSpeedY = speed * Input.GetAxis("Horizontal");
+            float curSpeedX = Input.GetAxisRaw("Vertical");
+            float curSpeedY = Input.GetAxisRaw("Horizontal");
 
             moveDirection = ((forward * curSpeedX) + (right * curSpeedY)).normalized;
-            moveDirection *= speed;
+            //moveDirection *= speed;
             child.transform.rotation = Quaternion.LookRotation(moveDirection * -1);
             Jump();
             //jump = false;
@@ -69,7 +69,7 @@ public class PlayerController : MonoBehaviour
 
 
         //Movement Speed
-        characterController.Move(moveDirection * Time.deltaTime);
+        characterController.Move(moveDirection * speed * Time.deltaTime);
 
         //Camera rotation
         /*if (canMove)
